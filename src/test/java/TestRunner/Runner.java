59f57933d8dbb96688fbd4646254a57cfcb648b0
Vasudev1994-droid <suryavasudev.g@gmail.com>
2026-05-02 13:59:54 +0530

    Project added

---
+++ src/test/java/TestRunner/Runner.java
@@ -0,0 +1,13 @@
+package TestRunner;
+
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features="src\\test\\resources\\Features",glue= {"Stepdefinitions"},
+dryRun=false,monochrome=true, plugin= {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
+
+public class Runner extends AbstractTestNGCucumberTests{
+
+}
